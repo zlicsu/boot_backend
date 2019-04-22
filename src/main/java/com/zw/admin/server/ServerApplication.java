7ed8131b7bd4b29a134a6138f14c43ecef07bd32
Ali <li.zhang7@dxc.com>
2019-04-22 12:37:17 +0800

    update to github

---
+++ src/main/java/com/zw/admin/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.zw.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 
+ * @author 小威老师 xiaoweijiagou@163.com
+ *
+ *         2017年4月18日
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
